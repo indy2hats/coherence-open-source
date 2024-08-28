a0e6fb22e0ce425a4e057d85c11f16cf816e164f
francis_sebin_fernandez <francis@2hatslogic.com>
2024-08-28 16:01:50 +0530

    laravel-vue-i18n package added for translation

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -5,6 +5,7 @@ import { createApp, h, DefineComponent } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/src/js';
+import { i18nVue } from 'laravel-vue-i18n';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -15,6 +16,18 @@ createInertiaApp({
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(i18nVue, {
+                resolve: async (lang: string) => {
+                    const langs = import.meta.glob('../../lang/*.json');
+
+                    // Make sure that the path is correctly resolved
+                    const langFile = langs[`../../lang/${lang}.json`];
+
+                    if (langFile) {
+                        return await langFile();
+                    }
+                }
+            })
             .mount(el);
     },
     progress: {
